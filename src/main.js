fb1c64c8ad3d33ed2851c47d77d2c31363186f49
Georgii Savin <gsavin@devexperts.com>
2021-03-02 18:53:28 +0300

    fix arrow functions

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 const rows = 30;
 const cols = 30;
+const currGen = [rows];
+const nextGen = [rows];
 
-let started = false;// Set to true when use clicks start
-let timer;//To control evolutions
-const EVOLUTION_SPEED = 1000;// One second between generations
+let started = false;
+let timer;
+const EVOLUTION_SPEED = 1000;
 
 function cellClick() {
     let loc = this.id.split("_");
-    let row = Number(loc[0]); //Get i
-    let col = Number(loc[1]); //Get j
+    let row = Number(loc[0]);
+    let col = Number(loc[1]);
     if (this.className === 'alive') {
         this.setAttribute('class', 'dead');
         currGen[row][col] = 0;
@@ -18,11 +20,147 @@ function cellClick() {
     }
 }
 
-function createWorld() {
-    const world = document.querySelector('#world');
-    const tbl = document.createElement('table');
-    tbl.setAttribute('id', 'world-grid');
+const getNeighborCount = (cellRow, cellCol) => {
+    let count = 0;
+    let nRow = Number(cellRow);
+    let nCol = Number(cellCol);
 
+    const isNotFirstRow = (row) => row - 1 >= 0
+    const isNotFirstCol = (col) => col - 1 >= 0
+    const isNotLastCol = (col) => col + 1 < cols
+    const isNotLastRow = (row) => row + 1 < rows
+
+    if (isNotFirstRow(nRow)) {
+        if (currGen[nRow - 1][nCol] === 1)
+            count++;
+    }
+
+    // check if not upper left corner
+    if (isNotFirstRow(nRow) && isNotFirstCol(nCol)) {
+        if (currGen[nRow - 1][nCol - 1] === 1)
+            count++;
+    }
+    // check if not upper right corner
+    if (isNotFirstRow(nRow) && isNotLastCol(nCol)) {
+        if (currGen[nRow - 1][nCol + 1] === 1)
+            count++;
+    }
+    // Make sure we are not on the first column
+    if (isNotFirstCol(nCol)) {
+        //Check left neighbor
+        if (currGen[nRow][nCol - 1] === 1)
+            count++;
+    }
+    // Make sure we are not on the last column
+    if (isNotLastCol(nCol)) {
+        //Check right neighbor
+        if (currGen[nRow][nCol + 1] === 1)
+            count++;
+    }
+    // Make sure we are not on the bottom left corner
+    if (isNotLastRow(nRow) && isNotFirstCol(nCol)) {
+        //Check bottom left neighbor
+        if (currGen[nRow + 1][nCol - 1] === 1)
+            count++;
+    }
+
+    if (isNotLastRow(nRow) && isNotLastCol(nCol)) {
+        //Check bottom right neighbor
+        if (currGen[nRow + 1][nCol + 1] === 1)
+            count++;
+    }
+
+    if (isNotLastRow(nRow)) {
+        if (currGen[nRow + 1][nCol] === 1)
+            count++;
+    }
+
+    return count;
+}
+
+const createNextGen = () => {
+    for (const row in currGen) {
+        for (const col in currGen[row]) {
+            const neighbors = getNeighborCount(row, col);
+            if (currGen[row][col] === 1) {
+                if (neighbors < 2) {
+                    nextGen[row][col] = 0;
+                } else if (neighbors === 2 || neighbors === 3) {
+                    nextGen[row][col] = 1;
+                } else if (neighbors > 3) {
+                    nextGen[row][col] = 0;
+                }
+            } else if (currGen[row][col] === 0) {
+                if (neighbors === 3) {
+                    nextGen[row][col] = 1;
+                }
+            }
+        }
+    }
+}
+
+const updateCurrGen = () => {
+    for (const row in currGen) {
+        for (const col in currGen[row]) {
+            currGen[row][col] = nextGen[row][col];
+            // Set nextGen back to empty
+            nextGen[row][col] = 0;
+        }
+    }
+
+}
+
+const updateWorld = () => {
+    for (const row in currGen) {
+        for (const col in currGen[row]) {
+            const cell = document.getElementById(row + '_' + col);
+            if (currGen[row][col] === 0) {
+                cell.setAttribute('class', 'dead');
+            } else {
+                cell.setAttribute('class', 'alive');
+            }
+        }
+    }
+}
+
+const startGame = (btn) => {
+    started = true;
+    btn.value = 'Stop Reproducing';
+    evolve();
+}
+
+const stopGame = (btn) => {
+    started = false;
+    btn.value = 'Start Reproducing';
+    clearTimeout(timer);
+}
+
+
+const startOrStopGame = () => {
+    const startStopBtn = document.querySelector('#btn-start-stop');
+    if (!started) {
+        startGame(startStopBtn);
+    } else {
+        stopGame(startStopBtn);
+    }
+}
+
+const evolve = () => {
+    createNextGen(); // Apply the game rules
+    updateCurrGen(); // Set Current values from new generation
+    updateWorld(); // Update the view
+    if (started) {
+        timer = setTimeout(evolve, EVOLUTION_SPEED);
+    }
+}
+
+const resetWorld = () => {
+    location.reload();
+}
+
+// initWorld Module start
+
+const createWorldCells = (tbl) => {
     for (let i = 0; i < rows; i++) {
         const tr = document.createElement('tr');
         for (let j = 0; j < cols; j++) {
@@ -34,21 +172,24 @@ function createWorld() {
         }
         tbl.appendChild(tr);
     }
+}
+
+const createWorld = () => {
+    const world = document.querySelector('#world');
+    const tbl = document.createElement('table');
+    tbl.setAttribute('id', 'world-grid');
+    createWorldCells(tbl);
     world.appendChild(tbl);
 }
 
-let currGen = [rows];
-let nextGen = [rows];
-
-// Creates two-dimensional arrays
-function createGenArrays() {
+const createGenerationArrays = () => {
     for (let i = 0; i < rows; i++) {
         currGen[i] = new Array(cols);
         nextGen[i] = new Array(cols);
     }
 }
 
-function initGenArrays() {
+const initGenArrays = () => {
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             currGen[i][j] = 0;
@@ -57,158 +198,14 @@ function initGenArrays() {
     }
 }
 
-function getNeighborCount(row, col) {
-    let count = 0;
-    let nrow = Number(row);
-    let ncol = Number(col);
-
-    // Make sure we are not at the first row
-    if (nrow - 1 >= 0) {
-        // Check top neighbor
-        if (currGen[nrow - 1][ncol] == 1)
-            count++;
-    }
-    // Make sure we are not in the first cell
-    // Upper left corner
-    if (nrow - 1 >= 0 && ncol - 1 >= 0) {
-        //Check upper left neighbor
-        if (currGen[nrow - 1][ncol - 1] == 1)
-            count++;
-    }
-// Make sure we are not on the first row last column
-    // Upper right corner
-    if (nrow - 1 >= 0 && ncol + 1 < cols) {
-        //Check upper right neighbor
-        if (currGen[nrow - 1][ncol + 1] == 1)
-            count++;
-    }
-// Make sure we are not on the first column
-    if (ncol - 1 >= 0) {
-        //Check left neighbor
-        if (currGen[nrow][ncol - 1] == 1)
-            count++;
-    }
-    // Make sure we are not on the last column
-    if (ncol + 1 < cols) {
-        //Check right neighbor
-        if (currGen[nrow][ncol + 1] == 1)
-            count++;
-    }
-// Make sure we are not on the bottom left corner
-    if (nrow + 1 < rows && ncol - 1 >= 0) {
-        //Check bottom left neighbor
-        if (currGen[nrow + 1][ncol - 1] == 1)
-            count++;
-    }
-// Make sure we are not on the bottom right
-    if (nrow + 1 < rows && ncol + 1 < cols) {
-        //Check bottom right neighbor
-        if (currGen[nrow + 1][ncol + 1] == 1)
-            count++;
-    }
-
-
-    // Make sure we are not on the last row
-    if (nrow + 1 < rows) {
-        //Check bottom neighbor
-        if (currGen[nrow + 1][ncol] == 1)
-            count++;
-    }
-
-
-    return count;
+const initWorld = () => {
+    createWorld();
+    createGenerationArrays();
+    initGenArrays();
 }
 
-function createNextGen() {
-    for (row in currGen) {
-        for (col in currGen[row]) {
-
-            let neighbors = getNeighborCount(row, col);
-
-            // Check the rules
-            // If Alive
-            if (currGen[row][col] == 1) {
-
-                if (neighbors < 2) {
-                    nextGen[row][col] = 0;
-                } else if (neighbors == 2 || neighbors == 3) {
-                    nextGen[row][col] = 1;
-                } else if (neighbors > 3) {
-                    nextGen[row][col] = 0;
-                }
-            } else if (currGen[row][col] == 0) {
-                // If Dead or Empty
-
-                if (neighbors == 3) {
-                    // Propogate the species
-                    nextGen[row][col] = 1;//Birth?
-                }
-            }
-        }
-    }
-
-}
-
-function updateCurrGen() {
-
-    for (row in currGen) {
-        for (col in currGen[row]) {
-            // Update the current generation with
-            // the results of createNextGen function
-            currGen[row][col] = nextGen[row][col];
-            // Set nextGen back to empty
-            nextGen[row][col] = 0;
-        }
-    }
-
-}
-
-function updateWorld() {
-    let cell = '';
-    for (row in currGen) {
-        for (col in currGen[row]) {
-            cell = document.getElementById(row + '_' + col);
-            if (currGen[row][col] == 0) {
-                cell.setAttribute('class', 'dead');
-            } else {
-                cell.setAttribute('class', 'alive');
-            }
-        }
-    }
-}
-
-function startStopGol() {
-    let startstop = document.querySelector('#btnstartstop');
-
-    if (!started) {
-        started = true;
-        startstop.value = 'Stop Reproducing';
-        evolve();
-
-    } else {
-        started = false;
-        startstop.value = 'Start Reproducing';
-        clearTimeout(timer);
-    }
-}
-
-function evolve() {
-
-    createNextGen();//Apply the rules
-    updateCurrGen();//Set Current values from new generation
-    updateWorld();//Update the world view
-
-    if (started) {
-        timer = setTimeout(evolve, EVOLUTION_SPEED);
-    }
-}
-
-function resetWorld() {
-    location.reload();
-}
+// initWorld Module end
 
 window.onload = () => {
-    createWorld();
-    createGenArrays();// current and next generations
-    initGenArrays();//Set all array locations to 0=dead
+    initWorld();
 }
